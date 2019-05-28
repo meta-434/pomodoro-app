b93d8752cbd77a197c8f33004242ff9ff5e91145
Alex H <acyrush@gmail.com>
2019-05-28 16:44:35 -0400

    add timer component

---
+++ pomodoro-app/src/components/Timer.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Timer from "react-compound-timer";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+  render() {
+    <Timer
+    initialTime={55000}
+    direction="backward"
+>
+    {() => (
+        <React.Fragment>
+            <Timer.Days /> days
+            <Timer.Hours /> hours
+            <Timer.Minutes /> minutes
+            <Timer.Seconds /> seconds
+            <Timer.Milliseconds /> milliseconds
+        </React.Fragment>
+    )}
+</Timer>);
+  }
+}
