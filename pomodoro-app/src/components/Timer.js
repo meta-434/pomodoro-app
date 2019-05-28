3ae29cddf648555eab71829faf537b0e6e6b7b36
Alex H <acyrush@gmail.com>
2019-05-28 16:54:00 -0400

    timer fix

--- pomodoro-app/src/components/Timer.js
+++ pomodoro-app/src/components/Timer.js
@@ -10,19 +10,18 @@ class Timer extends Component {
     };
   }
   render() {
-    <Timer
-    initialTime={55000}
-    direction="backward"
->
-    {() => (
-        <React.Fragment>
+    return (
+      <Timer initialTime={55000} direction="backward">
+        {() => (
+          <React.Fragment>
             <Timer.Days /> days
             <Timer.Hours /> hours
             <Timer.Minutes /> minutes
             <Timer.Seconds /> seconds
             <Timer.Milliseconds /> milliseconds
-        </React.Fragment>
-    )}
-</Timer>);
+          </React.Fragment>
+        )}
+      </Timer>
+    );
   }
 }
