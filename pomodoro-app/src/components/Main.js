1cc5d15404de8454808676c80d7330cb6228e93b
Alex <acyrush@gmail.com>
2019-05-28 23:34:22 -0400

    Timer functionality added
    Timer displays correctly.

--- pomodoro-app/src/components/Main.js
+++ pomodoro-app/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
+//firebase
 import firebase from "./firebase.js";
-
+//styling (material ui)
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -8,8 +9,14 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import { withStyles } from "@material-ui/core/styles";
-import Time from "./Timer.js";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+import "typeface-roboto";
+//timer
+import Time from "./Time.js";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,6 +25,20 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <Time />;
+    return (
+      <Container maxWidth="lg">
+        <Paper>
+          <Typography variant="h2">Pomodoro Tracker</Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            25 on, 5 off
+          </Typography>
+          <Card>
+            <CardContent>
+              <Time />
+            </CardContent>
+          </Card>
+        </Paper>
+      </Container>
+    );
   }
 }
---
+++ pomodoro-app/src/components/Time.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Timer from "react-compound-timer";
+import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
+
+class Time extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  render() {
+    return (
+      <Timer
+        initialTime={1500000}
+        direction="backward"
+        startImmediately={false}
+        onReset={() => console.log("onReset hook")} //use to switch to 5 minute timer (use a state or prop change?)
+        checkpoints={[
+          {
+            time: 1200000, //20 minutes
+            callback: () => console.log("20 minutes left")
+          },
+          {
+            time: 900000, // 15 minutes
+            callback: () => console.log("15 minutes left")
+          },
+          {
+            time: 600000, // 10 minutes
+            callback: () => console.log("10 minutes left")
+          },
+          {
+            time: 300000, // 5 minutes
+            callback: () => console.log("5 minutes left")
+          }
+        ]}
+      >
+        {({ start, resume, pause, stop, reset, timerState }) => (
+          <React.Fragment>
+            <div>
+              <Timer.Minutes /> minutes <br />
+              <Timer.Seconds /> seconds 
+            </div>
+            <div>
+              <Chip label={timerState} />
+            </div>
+            <br />
+            <div>
+              <Button variant="contained" color="primary" onClick={start}>
+                Start
+              </Button>
+              <Button onClick={pause}> Pause </Button>
+              <Button onClick={resume}> Resume </Button>
+              <Button variant="contained" color="secondary" onClick={stop}>
+                Stop
+              </Button>
+              <Button variant="outlined" color="primary" onClick={reset}>
+                Reset
+              </Button>
+            </div>
+          </React.Fragment>
+        )}
+      </Timer>
+    );
+  }
+}
+
+export default Time;
