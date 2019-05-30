141ac44a20bcb7f0d94ae40f983b7d9fa967ff22
meta-434 <ach1@vt.edu>
2019-05-30 14:34:20 -0400

    Auto stash before merge of "f-auth" and "origin/f-auth"

--- pomodoro-app/src/components/Login.js
+++ pomodoro-app/src/components/Login.js
@@ -5,23 +5,54 @@ import "firebase/auth";
 import firebaseApp from "./firebaseConfig";
 import logo from "../logo.svg";
 import "../App.css";
+import { Container, Paper, Card, Chip, Button } from "@material-ui/core";
 
 // const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const useStyles = makeStyles({
+//   card: {
+//     minWidth: 275,
+//   },
+//   bullet: {
+//     display: 'inline-block',
+//     margin: '0 2px',
+//     transform: 'scale(0.8)',
+//   },
+//   title: {
+//     fontSize: 14,
+//   },
+//   pos: {
+//     marginBottom: 12,
+//   },
+// });
 
 class Login extends Component {
   render() {
     const { user, signOut, signInWithGoogle } = this.props;
 
     return (
-      <div className="App">
-        {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
-
-        {user ? (
-          <button onClick={signOut}>Sign out</button>
-        ) : (
-          <button onClick={signInWithGoogle}>Sign in with Google</button>
-        )}
-      </div>
+      <Container maxWidth="sm">
+        <Paper>
+          <p>{"\n"}</p>
+          <Card>
+          <p>{"\n"}</p>
+            <Chip
+              label={
+                user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>
+              }
+            />
+            <p>{"\n"}</p>
+            
+            {user ? (
+              <Button variant="outlined" onClick={signOut}>Sign out</Button>
+            ) : (
+              <Button variant="outlined" onClick={signInWithGoogle}>Sign in with Google</Button>
+            )}
+            <p>{"\n"}</p>
+          </Card>
+          <p>{"\n"}</p>
+        </Paper>
+      </Container>
     );
   }
 }
