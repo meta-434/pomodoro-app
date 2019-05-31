2a1169a731554c4468dbef5c0412deb30674f0e7
Theodore Rose <ter9hb@virginia.edu>
2019-05-31 09:34:36 -0400

    merge this one!

--- pomodoro-app/src/components/Profile.js
+++ pomodoro-app/src/components/Profile.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Card, Container, Typography } from "@material-ui/core";
 import firebaseApp from "./firebaseConfig";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 class Profile extends Component {
   constructor(props) {
@@ -17,11 +22,27 @@ class Profile extends Component {
     if (onlyThisUser != null) {
       return (
         <Container maxWidth="md">
-        <Typography variant="h2">Pomodoro Tracker</Typography>
-        <Typography variant="h6">Completed Tasks:</Typography>
-          {Object.keys(onlyThisUser).map(key => {
-            return <Card>{JSON.stringify(onlyThisUser[key])} </Card>;
-          })}
+          <Typography variant="h2">Pomodoro Tracker</Typography>
+          <Typography variant="h6">Completed Tasks:</Typography>
+
+          <Table>
+            <TableHead>
+              <TableCell align="left">Entry</TableCell>
+              <TableCell align="right">Date Submitted</TableCell>
+            </TableHead>
+            {Object.keys(onlyThisUser).map(key => {
+              return (
+                <TableRow>
+                  <TableCell align="left">
+                    {onlyThisUser[key]["entry"]}
+                  </TableCell>
+                  <TableCell align="right">
+                    {onlyThisUser[key]["timestamp"]}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </Table>
         </Container>
       );
     }
